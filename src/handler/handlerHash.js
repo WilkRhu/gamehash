006db2850d8e8a3b350e52f406d6f8f7460c2a78
Wilk Franca <wilk.caetano@gmail.com>
2021-09-21 22:15:43 -0300

    feat: create function service verified

--- src/handler/handlerHash.js
+++ src/handler/handlerHash.js
@@ -1,10 +1,24 @@
 const app = require('../app')
 const handler = require('../../handler')
+const { verified } = require('../services')
 
 app.get('/', async(req, res, next) => {
     try {
         const { board } = req.query
-        return res.status(200).json(board.split(''))
+        const rows = []
+        const f = []
+        const s = []
+        const tr = []
+
+        for (let i = 0; i < board.length / 3; i++) {
+            const t = board.split('').splice(i * 3,  3);
+            rows.push(t)
+            f.push(t[0])
+            s.push(t[1])
+            tr.push(t[2])
+        }
+        const vf = await verified([f, s, tr], rows)
+       
     } catch (error) {
         throw error
     }
