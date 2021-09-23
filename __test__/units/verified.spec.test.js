2206c3dd71d341164cd8760619af49edeee30b48
Wilk Franca <wilk.caetano@gmail.com>
2021-09-23 10:16:43 -0300

    correction tests

--- __test__/units/verified.spec.test.js
+++ __test__/units/verified.spec.test.js
@@ -5,25 +5,25 @@ describe('Test handlerHash units', () => {
 
     it('should success return vf function rows', async () => {
         const {data, rows} = smashString(' oo x xox')
-        const responseRows = verifiedTypesPlays(data, rows)
+        const responseRows = await verifiedTypesPlays(data, rows)
         expect(responseRows).toBe('rows')
     })
 
     it('should success return vf function colum', async () => {
         const {data, rows} = smashString(' xxo  o  ')
-        const responseRows = verifiedTypesPlays(data, rows)
+        const responseRows = await verifiedTypesPlays(data, rows)
         expect(responseRows).toBe('colum')
     })
 
     it('should success return vf function diagonals', async () => {
         const {data, rows} = smashString('o  xo xx ')
-        const responseRows = verifiedTypesPlays(data, rows)
+        const responseRows = await verifiedTypesPlays(data, rows)
         expect(responseRows).toBe('diagonals')
     })
 
     it('should error return vf you loser', async () => {
         const {data, rows} = smashString('o  xo xxx ')
-        const responseRows = verifiedTypesPlays(data, rows)
+        const responseRows = await verifiedTypesPlays(data, rows)
         expect(responseRows.status).toBe(400)
         expect(responseRows.message).toBe('You Lose!')
     })
