0f77226315addeada26ddd0f7eb1fc8a2f7ef2db
Wilk Franca <wilk.caetano@gmail.com>
2021-09-24 14:36:28 -0300

    create tests

--- __test__/units/bestPlayerColum.test.js
+++ __test__/units/bestPlayerColum.test.js
@@ -1,24 +1,66 @@
 const { bestColumMoves, smashString } = require('../../src/services')
 describe('Test unit bestPlayerColum', () => {
-    it('should return success colum 1', () => {
+    it('should return success colum 1 0 - 0', () => {
         const board = ' xxo  o  '
         const {data, rows} = smashString(board)
         const resp = bestColumMoves(data, rows)
         expect(resp).toBe('oxxo  o  ')
     })
 
-    it('should return success colum 2', () => {
+    it('should return success colum 1 0 - 1', () => {
+        const board = 'oxx xoo  '
+        const {data, rows} = smashString(board)
+        const resp = bestColumMoves(data, rows)
+        expect(resp).toBe('oxxoxoo  ')
+    })
+
+    it('should return success colum 1 0 - 2', () => {
+        const board = 'oxxox   x'
+        const {data, rows} = smashString(board)
+        const resp = bestColumMoves(data, rows)
+        expect(resp).toBe('oxxox o x')
+    })
+
+    it('should return success colum 2 1 - 0', () => {
+        const board = 'x xooxxo '
+        const {data, rows} = smashString(board)
+        const resp = bestColumMoves(data, rows)
+        expect(resp).toBe('xoxooxxo ')
+    })
+
+    it('should return success colum 2 1 - 1', () => {
+        const board = 'xoxo xxo '
+        const {data, rows} = smashString(board)
+        const resp = bestColumMoves(data, rows)
+        expect(resp).toBe('xoxooxxo ')
+    })
+
+    it('should return success colum 2 1 - 2', () => {
         const board = 'xoxooxx  '
         const {data, rows} = smashString(board)
         const resp = bestColumMoves(data, rows)
         expect(resp).toBe('xoxooxxo ')
     })
 
-    it('should return success colum 3', () => {
-        const board = 'xx  xoo o'
+    it('should return success colum 3 2 - 0', () => {
+        const board = 'xx xoo xo'
         const {data, rows} = smashString(board)
         const resp = bestColumMoves(data, rows)
-        expect(resp).toBe('xxo xoo o')
+        expect(resp).toBe('xxoxoo xo')
+    })
+
+    it('should return success colum 3 2 - 1', () => {
+        const board = 'xxo o xxo'
+        const {data, rows} = smashString(board)
+        const resp = bestColumMoves(data, rows)
+        expect(resp).toBe('xxo ooxxo')
+    })
+
+    it('should return success colum 3 2 - 2', () => {
+        const board = 'xxoxooxx '
+        const {data, rows} = smashString(board)
+        const resp = bestColumMoves(data, rows)
+        expect(resp).toBe('xxoxooxxo')
     })
 
     it('should return error', () => {
